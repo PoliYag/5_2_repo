0e90c526f935790797c017e224c12d3e0002f2dd
PoliYag <150130450+PoliYag@users.noreply.github.com>
2023-12-07 18:00:57 +0300

    task_5_2

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создание и заполнение двумерного массива
+        int[,] array = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int rows = array.GetLength(0); // количество строк
+        int cols = array.GetLength(1); // количество столбцов
+
+        // Вывод исходного массива
+        Console.WriteLine("Исходный массив:");
+        PrintArray(array);
+
+        // Меняем местами первую и последнюю строку
+        for (int i = 0; i < cols; i++)
+        {
+            int temp = array[0, i];
+            array[0, i] = array[rows - 1, i];
+            array[rows - 1, i] = temp;
+        }
+
+        // Вывод измененного массива
+        Console.WriteLine("Измененный массив:");
+        PrintArray(array);
+    }
+
+    // Метод для вывода массива
+    static void PrintArray(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
